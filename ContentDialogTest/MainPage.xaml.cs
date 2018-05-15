0ed7ec66088150fd24d312a3473abca869d23cd1
Benjamin <schwendner@axinom.de>
2018-05-15 15:54:10 +0200

    Add project files.

---
+++ ContentDialogTest/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using SkyUK_UWP.Services.OverlayService.Dialogs.SignIn;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ContentDialogTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            await (new SignInDialog() { MinHeight = Window.Current.Bounds.Height }).ShowAsync();
+        }
+    }
+}
---
+++ ContentDialogTest/SignInDialog.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace SkyUK_UWP.Services.OverlayService.Dialogs.SignIn
+{
+    public sealed partial class SignInDialog : ContentDialog
+    {
+        public bool IsCancelled = false;
+
+        public SignInDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
